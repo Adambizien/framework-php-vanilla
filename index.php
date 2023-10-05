af34b8c0e27de17770708420141938b10d15ec74
Adambizien/blog-symfony <bizienadam@outlook.com>
2023-10-05 11:13:41 +0200

    update : initialisation

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+    require_once './configs/bootstrap.php';
+    // require_once  './src/toolkit.php'; pour l'appeller une seule fois 
+    // include  './src/toolkit.php'; include quand c'est pas obliger de mettre sa 
+?>
